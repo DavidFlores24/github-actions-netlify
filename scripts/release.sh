5a2d9d0fb09721e4beb20d84823278867347272a
David Flores <david@audiostack.ai>
2024-05-01 14:18:33 +0100

    build: add release script

---
+++ scripts/release.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo: "\nStarting Release script."
+
+echo -e "\nSetting current machine state to clean."
+git reset HEAD --hard
+git clean -xdf
+
+echo -e "\nPulling latest changes."
+git pull
+
+echo -e "\nInstalling & Building Project."
+# These two steps are not fully necessary as they happen on the pipeline BUT they would shout any issues BEFORE tagging the release
+npm install
+npm run build
+
+npm run dry_release
+
+echo -e "\n👀 Do the above changes look correct? (y/n)"
+read -tr 60 key
+if [[ $key == 'y' || $key == 'Y' ]]; then
+  npm run release
+else
+  echo -e "\n No release is being created"
+  return 1
+fi
+
+git push --follow-tags origin main
+return 0
